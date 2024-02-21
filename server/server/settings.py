fc241540c38afb53a3b3a93f615c528c281f5a58
Kenan Sultanic <kenansultanic0805@gmail.com>
2024-02-21 18:18:31 +0100

    updated media url

--- server/server/settings.py
+++ server/server/settings.py
@@ -147,7 +147,7 @@ USE_TZ = True
 STATIC_URL = 'staticfiles/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'staticfiles'),)
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'staticfiles')
-MEDIA_URLS = '/media/'
+MEDIA_URLS = '/images/'
 #MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Additional directories where static files are located
